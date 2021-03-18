c926576f7fa9214660f9a7827f579589527f46ce
Daniel Westfall <76882475+danielwestfall@users.noreply.github.com>
2021-03-18 18:12:56 -0500

    Update eyes.js

--- eyes.js
+++ eyes.js
@@ -11,3 +11,16 @@ document.onmousemove = (event) => {
   };
  
 };
+
+
+document.ontouchmove = (event) => {
+  const x = (event.clientX * 100) / window.innerWidth + '%';
+  const y = (event.clientY * 100) / window.innerHeight + '%';
+
+  for (let i = 0; i < 2; i++){
+      balls[i].style.left = x;
+      balls[i].style.top = y;
+      balls[i].transform = 'translate(-' + x + ',-' + y + ')';
+  };
+
+};
