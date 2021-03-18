20ad1fab91fd0defbcbe09c4a9fb5da0947c08ed
Daniel Westfall <76882475+danielwestfall@users.noreply.github.com>
2021-03-18 18:26:07 -0500

    Update eyes.js

--- eyes.js
+++ eyes.js
@@ -1,8 +1,8 @@
 const balls = document.getElementsByClassName('ball');
 
 document.ontouchmove = (event) => {
-  const x = (event.clientX * 100) / window.innerWidth + '%';
-  const y = (event.clientY * 100) / window.innerHeight + '%';
+  const x = (event.touches.clientX * 100) / window.innerWidth + '%';
+  const y = (event.touches.clientY * 100) / window.innerHeight + '%';
 
   for (let i = 0; i < 2; i++){
       balls[i].style.left = x;
